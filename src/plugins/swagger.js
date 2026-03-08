62506b1d02ae07047934b7b1390e2ba4be783136
[6212960]Chatmongkol(TOP)-DevSup <53871731+ChatmongkolTop@users.noreply.github.com>
2026-03-09 01:35:27 +0700

    Initial commit: check expired

--- src/plugins/swagger.js
+++ src/plugins/swagger.js
@@ -5,8 +5,8 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Domo API Documentation 09-03-2026(1.0.12)',
-      version: '1.0.12',
+      title: 'Domo - API Documentation 09-03-2026(1.0.14)',
+      version: '1.0.14',
       description: 'ตัวอย่าง API พร้อมโครงสร้างที่สะอาด',
     },
    servers: [
